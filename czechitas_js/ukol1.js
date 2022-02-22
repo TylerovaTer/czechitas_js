e954c39097103ab37e8fb14f4f58e8bc85833764
TylerovaTer <tereza.tylerova@seznam.cz>
2022-02-22 17:36:15 +0100

    1 file modified

--- czechitas_js/ukol1.js
+++ czechitas_js/ukol1.js
@@ -1,1 +1,6 @@
+//vypisuje text v uvozovce
 console.log('Ahoj');
+// vyžádá odpověď od uživetele
+//prompt("Kolik je ti let?");
+// zobrazí vyskakovací okýnko s textem
+alert("Tvá stránka je nejlepší")
