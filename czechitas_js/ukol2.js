d2e3fbbaa05d2209820aeffadf81f9be3d503f27
TylerovaTer <tereza.tylerova@seznam.cz>
2022-02-23 11:47:21 +0100

    1 file modified

--- czechitas_js/ukol2.js
+++ czechitas_js/ukol2.js
@@ -9,4 +9,4 @@ let rozdil_roky = (rok_zena - rok_muz)*12
 let rozdil_mesice = mesic_zena - mesic_muz;
 let rozdil_v_mesicich = rozdil_roky - Math.abs(rozdil_mesice)
 
-console.log(jmeno_zena + "je o " + Math.floor(rozdil_v_mesicich/12) + " let a " + rozdil_v_mesicich%12 + " mesicu mladsi nez " + jmeno_muz);
+console.log(jmeno_zena + " je o " + Math.floor(rozdil_v_mesicich/12) + " let a " + rozdil_v_mesicich%12 + " mesicu mladsi nez " + jmeno_muz);
