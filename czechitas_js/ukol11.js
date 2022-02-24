125506aa3662de4726d221ca56c96624a13df9e6
TylerovaTer <tereza.tylerova@seznam.cz>
2022-02-24 18:18:59 +0100

    1 file modified

--- czechitas_js/ukol11.js
+++ czechitas_js/ukol11.js
@@ -1,5 +1,5 @@
-let a = Number(prompt("zadej horní mez"))
+let a = Number(prompt('zadej horní mez'));
 
- for (let x = 0 ; x<=a && x%2===0 ; ) {
-   console.log(x)
- }
+for (let x = 0; x <= a && x % 2 === 0; x++) {
+  console.log(x);
+}
