39486d5a6124d36e0bee9cbe0b8e7059af31fee0
TylerovaTer <tereza.tylerova@seznam.cz>
2022-02-27 16:10:55 +0100

    2 files added, 1 deleted

---
+++ czechitas_js/ukol19.js
@@ -0,0 +1,16 @@
+let osoba1 = {
+  jmeno : tereza
+  prijmeni : tylerova
+  vek : 25
+  znamky: [1,2,4,,5]
+}
+
+let osoba2 = {
+  jmeno : Tomas
+  prijmeni : tyler
+  vek : 25
+  znamky : [1,1,2,2]
+}
+
+console.log(osoba1
+  )
